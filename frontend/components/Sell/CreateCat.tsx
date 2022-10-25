850c6bffcec90c2ac65df827342b40fe2d30e70e
erratinsilentio <kacper.kondracki@wp.pl>
2022-10-25 21:42:53 +0200

    form styling draft

--- frontend/components/Sell/CreateCat.tsx
+++ frontend/components/Sell/CreateCat.tsx
@@ -1,6 +1,23 @@
 import gql from 'graphql-tag';
 import { useMutation } from '@apollo/client';
+import styled from 'styled-components';
 import useForm from '../../utils/useForm';
+import Form from './Form';
+
+const Container = styled.div`
+  width: 100%;
+  height: 80%;
+  margin-top: 5vh;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  .right {
+    width: 50vw;
+    height: 70vh;
+    outline: 1px solid black;
+  }
+`;
 
 const CREATE_CAT_MUTATION = gql`
   mutation CREATE_CAT_MUTATION(
@@ -40,64 +57,67 @@ export default function CreateCat() {
     }
   );
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        console.log(inputs);
-        await createCat({
-          variables: inputs,
-        });
-        console.log(data);
-        clearForm();
-      }}
-    >
-      {/* <DisplayError error={error} /> */}
-      <fieldset disabled={loading} aria-busy={loading}>
-        <label htmlFor="image">
-          Image
-          <input
-            required
-            type="file"
-            name="image"
-            id="image"
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="name"
-            value={inputs.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="price">
-          Price
-          <input
-            type="number"
-            name="price"
-            id="price"
-            placeholder="price"
-            value={inputs.price}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="description">
-          Description
-          <textarea
-            type="text"
-            name="description"
-            id="description"
-            placeholder="description"
-            value={inputs.description}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">+ Add Cat</button>
-      </fieldset>
-    </form>
+    <Container>
+      <Form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          console.log(inputs);
+          await createCat({
+            variables: inputs,
+          });
+          console.log(data);
+          clearForm();
+        }}
+      >
+        {/* <DisplayError error={error} /> */}
+        <fieldset disabled={loading} aria-busy={loading}>
+          <label htmlFor="image">
+            Image
+            <input
+              required
+              type="file"
+              name="image"
+              id="image"
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="name"
+              value={inputs.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="price"
+              value={inputs.price}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="description">
+            Description
+            <textarea
+              type="text"
+              name="description"
+              id="description"
+              placeholder="description"
+              value={inputs.description}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit">+ Add Cat</button>
+        </fieldset>
+      </Form>
+      <div className="right" />
+    </Container>
   );
 }
