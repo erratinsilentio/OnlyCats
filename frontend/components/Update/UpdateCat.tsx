f32bf8d2b4986877ae1b757cc4f5271aef8a8444
erratinsilentio <kacper.kondracki@wp.pl>
2022-10-27 19:21:36 +0200

    added update form

--- frontend/components/Update/UpdateCat.tsx
+++ frontend/components/Update/UpdateCat.tsx
@@ -1,8 +1,9 @@
-import gql, { useMutation, useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
 import Router from 'next/router';
 import ErrorMessage from '../../utils/Error';
 import useForm from '../../utils/useForm';
-import Form from '../Sell/Form';
+import UpdateForm from './UpdateForm';
 
 const SINGLE_CAT_QUERY = gql`
   query SINGLE_CAT_QUERY($id: ID!) {
@@ -14,3 +15,103 @@ const SINGLE_CAT_QUERY = gql`
     }
   }
 `;
+
+const UPDATE_CAT_MUTATION = gql`
+  mutation UPDATE_CAT_MUTATION(
+    $id: ID!
+    $name: String
+    $description: String
+    $price: Int
+  ) {
+    updateCat(
+      id: $id
+      data: { name: $name, description: $description, price: $price }
+    ) {
+      id
+      name
+      description
+      price
+    }
+  }
+`;
+
+export default function UpdateCat({ id }) {
+  const { data, error, loading } = useQuery(SINGLE_CAT_QUERY, {
+    variables: {
+      id,
+    },
+  });
+
+  const [
+    updateCat,
+    { data: updateData, error: updateError, loading: updateLoading },
+  ] = useMutation(UPDATE_CAT_MUTATION, {
+    variables: {
+      id,
+    },
+  });
+
+  const { inputs, handleChange, clearForm } = useForm(data?.Cat);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <UpdateForm
+      onSubmit={async (e) => {
+        e.preventDefault();
+        const res = await updateCat({
+          variables: {
+            id,
+            name: inputs.name,
+            description: inputs.description,
+            price: inputs.price,
+          },
+        });
+        Router.push({
+          pathname: `/shop/${res.data.updateCat.id}`,
+        });
+      }}
+    >
+      <fieldset disabled={loading} aria-busy={loading}>
+        <ErrorMessage error={error} />
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="name"
+            value={inputs.name}
+            onChange={handleChange}
+            minLength="3"
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            name="price"
+            id="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+            min="1"
+            max="1000000"
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            type="text"
+            name="description"
+            id="description"
+            placeholder="description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">+ Add Cat</button>
+      </fieldset>
+    </UpdateForm>
+  );
+}
--- frontend/pages/update.tsx
+++ frontend/pages/update.tsx
@@ -1,6 +1,5 @@
 import UpdateCat from '../components/Update/UpdateCat';
 
 export default function ({ query }) {
-  //   return <UpdateCat id={query.id} />;
-  return <p>hello from update page</p>;
+  return <UpdateCat id={query.id} />;
 }
