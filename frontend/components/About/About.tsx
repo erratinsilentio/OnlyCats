cb719c80e1afb0ffc45b56eb2a58783b438f765f
erratinsilentio <kacper.kondracki@wp.pl>
2022-11-21 17:51:32 +0100

    added more styling and animations on about page

--- frontend/components/About/About.tsx
+++ frontend/components/About/About.tsx
@@ -10,6 +10,16 @@ const appear = keyframes`
   }
 `;
 
+const title = keyframes`
+  from {
+    color: var(--pall2);
+  }
+
+  to {
+    color: var(--pall1);
+  }
+`;
+
 const translate = keyframes`
   from {
     transform: translateX(-1000%);
@@ -17,6 +27,26 @@ const translate = keyframes`
 
   to {
     transform: translateX(0);
+  }
+`;
+
+const right3 = keyframes`
+  from {
+    right: -100%;
+  }
+
+  to {
+    right: -3%;
+  }
+`;
+
+const right5 = keyframes`
+  from {
+    right: -100%;
+  }
+
+  to {
+    right: 5%;
   }
 `;
 
@@ -61,24 +91,56 @@ export const Container = styled.div`
 
   .right {
     padding: 1rem;
+    position: relative;
   }
   h1 {
     font-size: 2rem;
-    margin-top: 1rem;
+    padding-left: 2rem;
+    padding-top: 1rem;
+    color: var(--pall1);
   }
   .content {
-    margin-top: 2rem;
+    margin-top: 4rem;
     padding: 1rem;
     width: 80%;
-    height: auto;
+    height: 80%;
     font-size: 1.2rem;
     line-height: 4vh;
-    border-top: 1px solid var(--pall4);
-    animation: ${appear} 1s;
+    /* border: 1px solid var(--blue1); */
+    box-shadow: 15px 10px 50px var(--pall4);
+    z-index: 10;
+    animation: ${appear} 2s;
+
+    background-color: var(--pall1);
   }
 
   p {
     margin: 1rem;
+  }
+
+  .aa {
+    width: 80%;
+    height: 80%;
+    position: absolute;
+    top: 5%;
+    right: 5%;
+    z-index: -1;
+    background-color: var(--pall2);
+    animation: ${appear} 2s;
+    animation: ${right5} 1s;
+    box-shadow: 15px 10px 40px var(--pall4);
+  }
+  .bb {
+    width: 80%;
+    height: 80%;
+    position: absolute;
+    top: -3%;
+    right: -3%;
+    z-index: -2;
+    background-color: var(--pall3);
+    animation: ${appear} 3s;
+    animation: ${right3} 0.5s;
+    box-shadow: 15px 10px 30px var(--pall4);
   }
 `;
 
@@ -90,7 +152,6 @@ export default function About() {
           <img src="../../static/ziutek.svg" />
         </div>
         <div className="right b">
-          <h1>_About</h1>
           <div className="content">
             <p>Hello World!</p>
             <p>
@@ -101,6 +162,10 @@ export default function About() {
               When I get to finally sit in peace and do some coding, he jumps on
               my lap and sleeps there, while I create.
             </p>
+            <div className="aa">
+              <h1>About</h1>
+            </div>
+            <div className="bb" />
           </div>
         </div>
       </div>
