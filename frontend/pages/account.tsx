af5fe9d8df3d4c52d08205c8e8e687c9cb5fa485
erratinsilentio <kacper.kondracki@wp.pl>
2022-11-25 19:01:40 +0100

    started working on account page

--- frontend/pages/account.tsx
+++ frontend/pages/account.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: 5vh;
+  width: 100vw;
+  height: 70vh;
+  outline: 1px solid black;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
 export default function Account() {
-  return <h1>account</h1>;
+  return (
+    <Container>
+      <h1>Account</h1>
+    </Container>
+  );
 }
