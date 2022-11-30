dce98a9cfd021eb1fc2a245ffa46c7f8dbdbc262
erratinsilentio <kacper.kondracki@wp.pl>
2022-11-30 11:27:07 +0100

    some styling for checkout page

--- frontend/components/Checkout/Checkout.tsx
+++ frontend/components/Checkout/Checkout.tsx
@@ -8,25 +8,37 @@ import { loadStripe } from "@stripe/stripe-js";
 import { useState } from "react";
 import styled from "styled-components";
 import nProgress from "nprogress";
+import { useUser } from "../../utils/useUser";
+import nameFormat from "../../utils/nameFormatter";
 
 const Container = styled.div`
-  outline: 1px solid black;
   margin-top: 10vh;
   width: 100vw;
   height: 70vh;
 
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
 
   .order {
+    font-family: "Courier New", Courier, monospace;
     width: 48vw;
+    border: 15px solid var(--pall3);
+    display: flex;
+    align-items: flex-start;
+  }
+
+  p {
+    font-size: 2rem;
+    padding-left: 1rem;
+    line-height: 7.5vh;
+    width: 46vw;
+    height: 7.5vh;
+    outline: 1px solid var(--pall3);
   }
 `;
 
 const CheckoutFormStyled = styled.form`
-  box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
-  border: 1px solid rgba(0, 0, 0, 0.06);
-  border-radius: 5px;
+  border: 1px solid var(--pall3);
   padding: 1rem;
   display: grid;
   grid-gap: 1rem;
@@ -36,6 +48,8 @@ const CheckoutFormStyled = styled.form`
     background-color: var(--pall3);
     border: 0px;
     color: white;
+    height: 5vh;
+    width: 45vw;
     cursor: pointer;
     &:hover {
       background-color: var(--pall2);
@@ -50,6 +64,10 @@ function CheckoutForm() {
   const [loading, setLoading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
+
+  const me = useUser();
+  const { cart } = me;
+  const { name } = me;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -66,16 +84,18 @@ function CheckoutForm() {
     if (error) {
       setError(error);
     }
+
+    nProgress.done();
   };
 
   return (
     <Container>
       <div className="order">
-        <p>order</p>
+        <p>{nameFormat(name)}'s Cart</p>
       </div>
       <CheckoutFormStyled onSubmit={handleSubmit}>
         {error && <p>{error.message}</p>}
-        <CardElement />
+        <CardElement id="card" />
         <button type="submit" value="submit">
           SUBMIT
         </button>
